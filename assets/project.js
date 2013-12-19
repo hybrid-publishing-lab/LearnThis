94c247b17ab762f9a80f2eebd18374d8d7929002
Niklas Wilcke <nw@folge3.de>
2013-12-19 16:02:35 +0100

    first try with bootstrap and jquery

---
+++ assets/project.js
@@ -0,0 +1,16 @@
+$(document).ready( function(){
+  // autogrow textareas
+  $('textarea').autogrow({onInitialize: true});
+
+  $('textarea').focus(function(event){
+    // Dimm all other text elements
+  });
+
+  $('.comment-button').click(function(event){
+    // insert comment textarea
+
+    // focus comment textarea
+
+    // replace comment button by delete comment button
+  });
+});
