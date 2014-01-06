acebd9b6475afe8bb7fa2dc789e9e0ce5b3fa465
Niklas Wilcke <nw@folge3.de>
2014-01-06 13:23:38 +0100

    javascript editmode started

--- assets/project.js
+++ assets/project.js
@@ -1,16 +1,88 @@
+// Leave Edit Mode
+$(document).mouseup(function (e){
+  var container = $(".active");
+  // if the target of the click isn't the container
+  // nor a descendant of the container
+  if (!container.is(e.target) && container.has(e.target).length === 0){
+      leaveEditMode();
+  }
+});
+
 $(document).ready( function(){
   // autogrow textareas
   $('textarea').autogrow({onInitialize: true});
 
+  // Focus a Textarea
   $('textarea').focus(function(event){
-    // Dimm all other text elements
+    leaveEditMode();
+    enterEditMode(this);
   });
 
+  // Focus a Headline
+  $('.headline').focus(function(event){
+    leaveEditMode();
+    enterEditMode(this);
+  });
+
+  // Comment Button
   $('.comment-button').click(function(event){
-    // insert comment textarea
+    var comment = $('textarea#comment');
+    if(comment.is(':hidden')){
+      // ŕeveal comment textarea and focus
+      comment.show().focus();
+      $('.comment-button').text('Delete Comment');
+    }else{
+      comment.hide();
+      comment.val('');
+      $('.comment-button').text('Comment');
+    }
+  });
 
-    // focus comment textarea
-
-    // replace comment button by delete comment button
+  // Add Text Button
+  $('.add-text-button').click(function(event){
+    var comment = $('textarea#comment');
+    if(comment.is(':hidden')){
+      // ŕeveal comment textarea and focus
+      comment.show().focus();
+      $('.comment-button').text('Delete Comment');
+    }else{
+      comment.hide();
+      comment.val('');
+      $('.comment-button').text('Comment');
+    }
   });
 });
+
+function dimmOtherTextElements(element){
+  var textElement = $(element).closest('.text-element');
+  $(".text-element").not(textElement).addClass('dimmed');
+}
+
+function unDimmTextElements(){
+  $(".text-element").removeClass('dimmed');
+}
+
+function removeMenues(){
+  $('.text-element').find('.show-on-focus').fadeOut();
+}
+
+function displayMenues(element){
+  var showElements = $(element).closest('.text-element').find('.show-on-focus');
+  showElements.fadeIn();
+}
+
+function enterEditMode(element){
+  // deactivate Editmode for all other text-elements
+  $('.text-element').removeClass('active');
+  $(element).closest('.text-element').addClass('active');
+
+  unDimmTextElements();
+  dimmOtherTextElements(element);
+
+  displayMenues(element);
+}
+
+function leaveEditMode(){
+  unDimmTextElements();
+  removeMenues();
+}
