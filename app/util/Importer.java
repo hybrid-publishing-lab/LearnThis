e8603a758a32f5af2fdf5a2dceddcfae0f2198e0
jd <jd@localhost>
2015-05-20 10:48:28 +0200

    Added freitext form field to import

--- app/util/Importer.java
+++ app/util/Importer.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,50 +15,66 @@ import models.MultipleChoice;
 
 import org.apache.commons.lang3.StringUtils;
 
+import play.Logger;
+
 public class Importer {
     public static Document importFromTextfile(File file, String encoding, boolean splitText) throws Exception{
         Document doc = new Document();
         BufferedReader br = null;
         try {
             br = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding));
-            List<String> textelements = parseTextelements(br, splitText);
-            int i = 0;
-            for(String element : textelements){
-//                if(element.length() <= 200){
-//                    // Headline
-//                    Headline hl = new Headline();
-//                    hl.text = element;
-//                    hl.size = 1;
-//                    Card card = new Card();
-//                    card.front = hl;
-//                    card.back = new MultipleChoice();
-//                    card.sort = i;
-//                    doc.cards.add(card);
-//                    card.document = doc;
-//                }else{
-                    // Paragraph
-                    MultipleChoice mc = new MultipleChoice();
-                    mc.text = element;
-                    Card card = new Card();
-                    card.sort = i;
-                    card.front = mc;
-                    card.back = new MultipleChoice();
-                    doc.cards.add(card);
-                    card.document = doc;
-//                }
-                i++;
-            }
-        }finally {
+            fillDocWithText(doc, br, splitText);
+        } catch(Exception e) {
+            Logger.error("Error importing Text from file " + file.getAbsolutePath(), e);
+        } finally {
             if (br != null) {
                 try {
                     br.close();
                 } catch (IOException e) {
+                    Logger.error("Error while closing BufferedReader", e);
                 }
             }
         }
         return doc;
     }
 
+    public static Document importFromText(String freitext, String encoding, boolean splitText) {
+        Document doc = new Document();
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new StringReader(freitext));
+            fillDocWithText(doc, br, splitText);
+        } catch(Exception e) {
+            Logger.error("Error importing Text: " + freitext, e);
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    Logger.error("Error while closing BufferedReader", e);
+                }
+            }
+        }
+        return doc;
+    }
+
+
+    private static void fillDocWithText(Document doc, BufferedReader br, boolean splitText) throws IOException {
+        List<String> textelements = parseTextelements(br, splitText);
+        int i = 0;
+        for(String element : textelements){
+            MultipleChoice mc = new MultipleChoice();
+            mc.text = element;
+            Card card = new Card();
+            card.sort = i;
+            card.front = mc;
+            card.back = new MultipleChoice();
+            doc.cards.add(card);
+            card.document = doc;
+            i++;
+        }
+    }
+    
     private static List<String> parseTextelements(BufferedReader br, boolean splitText) throws IOException {
 
         List<String> textelements = new ArrayList<String>();
@@ -89,5 +106,4 @@ public class Importer {
     private static Boolean isEmpty(String s) {
         return s.matches("\\s*");
     }
-
 }
