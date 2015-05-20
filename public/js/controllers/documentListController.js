138c97cd272a29d13617a63cad0f9b513a0ab7b8
jd <jd@localhost>
2015-05-20 17:50:52 +0200

    Made main page load in batches + added learnCount

--- public/js/controllers/documentListController.js
+++ public/js/controllers/documentListController.js
@@ -4,33 +4,52 @@ function DocumentListController($scope, $http, saveService){
 	
 	$scope.saved = true;
 	$scope.isInit = false;
+	$scope.page = 0;
+	$scope.hasAll = false;
 	
 	$scope.init = function(fetchAll){
 		if (!$scope.isInit) {
 			$scope.isInit = true;
-			var url = '/json/document/random/5';
-			if (fetchAll) {
-			  url = '/json/document/all';
-      }
-			$http.get(url).success(function(data){
-				$scope.documents = data;
-				for(j = 0; j < $scope.documents.length; ++j) {
-					var combinedTexts = "" + $scope.documents[j].title;
-					for(index = 0; index < $scope.documents[j].cards.length; ++index) {
-						var card = $scope.documents[j].cards[index];
-						if (card.front && card.front.text) {
-							combinedTexts += ' ' + card.front.text;
-						}
-						if (card.back && card.back.text) {
-							combinedTexts += ' ' + card.back.text;
-						}
-					}
-					$scope.documents[j].combinedTexts = combinedTexts;
-				}
-			});
+			$scope.fetchData(true);
 		}
 	}
 	
+	$scope.fetchData = function(fetchAll) {
+	  var url = '/json/document/random/5';
+	  if (fetchAll) {
+	    url = '/json/document/all/'+$scope.page;
+	  }
+	  $http.get(url).success(function(data){
+	    if (data && data.length > 0) {
+	      for(j = 0; j < data.length; ++j) {
+	        var combinedTexts = "" + data[j].title;
+	        for(index = 0; index < data[j].cards.length; ++index) {
+	          var card = data[j].cards[index];
+	          if (card.front && card.front.text) {
+	            combinedTexts += ' ' + card.front.text;
+	          }
+	          if (card.back && card.back.text) {
+	            combinedTexts += ' ' + card.back.text;
+	          }
+	        }
+	        data[j].combinedTexts = combinedTexts;
+	      }
+	      if ($scope.documents) {
+	        $scope.documents.push.apply($scope.documents, data);
+	      } else {
+	        $scope.documents = data;
+	      }
+      } else {
+        $scope.hasAll = true;
+      }
+	  });
+	}
+
+  $scope.fetchNext = function() {
+    $scope.page++;
+    $scope.fetchData(true);
+  }
+  
 	$scope.deleteElement = function (ele, index) {
 		$scope.saved = false;
 		$http.post('/json/document/delete/'+ele.id, ele).success(function(data){
