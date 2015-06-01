27fc3f5fd9b95007281b150d6858e7194092e2a0
jd <jd@localhost>
2015-06-01 16:30:15 +0200

    Fixed Cache Eviction

--- app/models/DocumentKeyGenerator.java
+++ app/models/DocumentKeyGenerator.java
@@ -1,6 +1,5 @@
 package models;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.springframework.cache.interceptor.KeyGenerator;
@@ -9,7 +8,6 @@ public class DocumentKeyGenerator implements KeyGenerator {
 
     @Override
     public Object generate(Object arg0, Method arg1, Object... arg2) {
-        Class<? extends Object> clazz = arg2[0].getClass();
         return ((Document)arg2[0]).id;
     }
 
