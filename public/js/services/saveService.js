dd385f834363b28c689a30c7e72f26030636fa84
jd <jd@localhost>
2015-06-01 15:43:20 +0200

    many small fixes

--- public/js/services/saveService.js
+++ public/js/services/saveService.js
@@ -1,4 +1,4 @@
-function SaveService($http){
+function SaveService($http, localStorageService){
 	
 	var me = this;
 	
@@ -16,10 +16,46 @@ function SaveService($http){
 			$scope.document.email = email;
 			$scope.saved = true;
 			$scope.currentlySaving = false;
+			if (!$scope.isPersisted()) {
+			  // Adressbar needs to change from /document/new to /document/id
+			  var title = "Kartenset: " + $scope.document.title + " - LearnThis";
+			  window.history.replaceState(title, title, "/document/edit/"+$scope.document.id);
+      }
 		})
 		.error(function(data, status, header, config){
 			$scope.currentlySaving = false;
 		});
 	}
-	
+
+  this.deleteDocument = function(id, pw) {
+    $http.post('/json/document/delete/'+id, {pw:pw}).
+    success(function(data, status, headers, config) {
+      window.location.replace("/");
+    }).
+    error(function(data, status, headers, config) {
+      alert("Passwort inkorrekt.");
+    });
+  }
+  
+  this.deleteDoc = function(id, title) {
+    // get doc from localStorage to check password
+    var savedResult = localStorageService.loadDoc(id);
+    util.log("savedResult", savedResult);
+    var password = '';
+    if (savedResult.password) {
+      password = savedResult.password.pw;
+    }
+
+    // check if password matches
+    $http.post('/json/document/checkpw/' + id, {pw:password})
+      .success(function (data, status, headers, config) {
+        if (confirm("Wollen Sie das Kartenset '"+title+"' wirklich löschen?")) {
+          me.deleteDocument(id, password);
+        }
+      }).error(function (data, status, headers, config) {
+        password = prompt("Zum Löschen des Kartensets '"+title+"' wird das Passwort benötigt.", "");
+        me.deleteDocument(id, password);
+      });
+  }
+    
 }
